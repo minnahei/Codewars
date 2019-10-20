697b09718a978362e8f644dfd3707b19720484f1
minnahei <minna.s.heinonen@gmail.com>
2019-10-20 13:18:13 +0200

    Added kata

---
+++ vowelCount/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace vowelCount
+{
+    class Program
+    {
+        public static int VowelCount(string str)
+        {
+            int vowelCount = 0;
+
+            char[] letters = str.ToCharArray();
+
+            foreach (char c in letters)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    vowelCount++;
+                }
+            }
+            return vowelCount;
+        }
+    }
+}
