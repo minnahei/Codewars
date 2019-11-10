40679952a4818ec0faeed9123699df6c7ad307af
minnahei <minna.s.heinonen@gmail.com>
2019-11-10 18:12:07 +0100

    Added kata

---
+++ ParityOutlier/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParityOutlier
+{
+    class Program
+    {
+        public static int Find(int[] integers)
+        {
+            var oddInts = new List<int>();
+            var evenInts = new List<int>();
+
+            foreach (int num in integers)
+            {
+                if (num % 2 == 0)
+                {
+                    evenInts.Add(num);
+                }
+                else
+                {
+                    oddInts.Add(num);
+                }
+            }
+
+            if (oddInts.Count > evenInts.Count)
+            {
+                return evenInts[0];
+            }
+            else
+            {
+                return oddInts[0];
+            }
+        }
+    }
+}
