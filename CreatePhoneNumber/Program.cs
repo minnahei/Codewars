b67d3d62e4a41d67947ab60607a34844b60116b6
minnahei <minna.s.heinonen@gmail.com>
2019-11-18 18:29:33 +0100

    Added kata

---
+++ CreatePhoneNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CreatePhoneNumber
+{
+    class Program
+    {
+        public static string CreatePhoneNumber(int[]numbers)
+        {
+            return "(" + numbers[0] + numbers[1] + numbers[2] + ")"
+                + numbers[3] + numbers[4] + numbers[5] + "-"
+                + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+        }
+    }
+}
