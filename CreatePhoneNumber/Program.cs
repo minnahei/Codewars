655df5e3d2833f013144a29d01fef71090742142
minnahei <minna.s.heinonen@gmail.com>
2019-11-18 18:34:11 +0100

    added description

--- CreatePhoneNumber/Program.cs
+++ CreatePhoneNumber/Program.cs
@@ -4,6 +4,11 @@ namespace CreatePhoneNumber
 {
     class Program
     {
+// Write a function that accepts an array of 10 integers(between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example:
+// Kata.CreatePhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+
         public static string CreatePhoneNumber(int[]numbers)
         {
             return "(" + numbers[0] + numbers[1] + numbers[2] + ")"
